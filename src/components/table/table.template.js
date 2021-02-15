da71933018bbfd8ec622fc9b10f4f716260aac4a
KarKaron <karkotski@gmail.com>
2021-02-15 09:23:41 +0300

    finish resize table

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -4,61 +4,80 @@ const CODES = {
   Z: 90
 }
 
-function toCell() {
+function toCell(_, col) {
   return `
-    <div 
+    <div
       class="cell"
       contenteditable="true"
       spellcheck="false"
+      data-col="${col}"
     >
     </div>
-  `
+  `;
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
   return `
-    <div class="column">
+    <div
+      class="column" 
+      data-type="resize" 
+      data-col="${index}"
+    >
       ${col}
+      <div
+        class="col-resize" 
+        data-resize="col"
+      ></div>
     </div>
-  `
+  `;
 }
 
 function createRow(index, content) {
+  const resize = index
+  ? `<div 
+      class="row-resize" 
+      data-resize="row"
+      ></div>
+    ` 
+  : '';
+
   return `
-    <div class="row">
+    <div 
+      class="row" 
+      data-type="resize"
+    >
       <div class="row-info">
         ${index ? index : ''}
+        ${resize}
       </div>
       <div class="row-data">
         ${content}
       </div>
     </div>
-  `
+  `;
 }
 
 function toChar(_, index) {
-  return String.fromCharCode(CODES.A + index)
+  return String.fromCharCode(CODES.A + index);
 }
 
 export function createTable(rowsCount = 15) {
-  const colsCount = CODES.Z - CODES.A +1
-  const rows = []
-
-  const cols = new Array(colsCount)
-      .fill('')
-      .map(toChar)
-      .map(toColumn)
-      .join('')
-
-  rows.push(createRow(null, cols))
+  const colsCount = CODES.Z - CODES.A + 1,
+    rows = [],
+    cols = new Array(colsCount)
+        .fill('')
+        .map(toChar)
+        .map(toColumn)
+        .join('');
+  rows.push(createRow(null, cols));
 
   for (let i =0; i < rowsCount; i++) {
     const cells = new Array(colsCount)
         .fill('')
         .map(toCell)
-        .join('')
-    rows.push(createRow(i + 1, cells))
+        .join('');
+    rows.push(createRow(i + 1, cells));
   }
 
-  return rows.join('')
+  return rows.join('');
 }
